f17ebf5475ae8543b03c35484d4c29c9478c4f3b
roberto furlan <furlanrgf@gmail.com>
2023-06-25 08:48:55 -0300

    🚀 add models Entity UsersProfile

---
+++ src/models/UsersProfile.ts
@@ -0,0 +1,56 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+
+export class UsersProfile extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  cellphone: number;
+
+  @Column()
+  phone: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  PJ: boolean;
+
+  @Column()
+  PF: boolean;
+
+  @Column()
+  CPF: number;
+
+  @Column()
+  CNPJ: number;
+
+  @Column()
+  CEP: number;
+
+  @Column()
+  address: string;
+
+  @Column()
+  number: number;
+
+  @Column()
+  complement: string;
+
+  @Column()
+  neighborhood: string;
+
+  @Column()
+  city: string;
+
+  @Column()
+  state: string;
+
+  @Column()
+  agreement: boolean;
+}
